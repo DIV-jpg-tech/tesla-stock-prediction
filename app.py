c45f4d0041067370ec89d8b5508558fd6d637d1c
Divyanshi Dhuliya <divyanshidhuliya@gmail.com>
2026-03-20 17:08:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
- LSTM, Dense, Dropout
 
 st.title("Tesla Stock Price Prediction")
 st.write("Predicting next 10 days stock price using LSTM")
